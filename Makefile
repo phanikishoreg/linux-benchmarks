6a0fec97e38cb8f3e144520531faabd9c480af33
Phani <phanikishoreg@gwu.edu>
2019-05-24 13:48:18 -0400

    passive costs test

--- Makefile
+++ Makefile
@@ -2,10 +2,16 @@ CC := gcc
 RM := rm
 CFLAGS := -O3 -g
 
-all: pipe_ipc pipe_rpc threads
+all: pipe_ipc_proc pipe_ipc_thd
 
-%: %.c priority.c
-	$(CC) $(CFLAGS) $< priority.c -o $@.out -lrt -lpthread
+pipe_ipc_proc: pipe_ipc.c priority.c
+	$(CC) $(CFLAGS) -DUSE_FORK pipe_ipc.c priority.c -o $@.out -lrt -lpthread
+
+pipe_ipc_thd: pipe_ipc.c priority.c
+	$(CC) $(CFLAGS) pipe_ipc.c priority.c -o $@.out -lrt -lpthread
+
+#%: %.c priority.c
+#	$(CC) $(CFLAGS) $< priority.c -o $@.out -lrt -lpthread
 
 clean:
 	$(RM) -f *.o *.out 
--- pipe_ipc.c
+++ pipe_ipc.c
@@ -12,12 +12,29 @@
 #include <assert.h>
 
 #define USEC_WAIT   100
-#define ITERS       100
+#define ITERS       1000000
 
 #define CHILD_CPU   0
 #define CHILD_PRIO  0
 #define PARENT_CPU  0
 #define PARENT_PRIO 1
+
+#define CACHE_LINE_SIZE 64
+#define WORKSET_SIZE_MIN CACHE_LINE_SIZE
+#define WORKSET_SIZE_MAX (12*1024*1024)
+
+#define WORKSET_32K 512 //L1 data
+#define WORKSET_256K 4096 //L2
+#define WORKSET_12MB 196608 //L3
+
+#define WORKSET_TEST_SIZE WORKSET_12MB /* power of 2, upto 12MB */
+
+#define CACHE_LINE_ULL_NELE (CACHE_LINE_SIZE/sizeof(unsigned long long))
+struct workset_data {
+	unsigned long long n; /* next array item to access */
+} __attribute__((aligned(64)));
+
+static struct workset_data ws[WORKSET_TEST_SIZE] __attribute__((aligned(4096)));
 
 extern void set_prio (unsigned int, int);
 
@@ -29,23 +46,140 @@ rdtsc (void)
   return x;
 }
 
+static inline void
+generate_workset(void)
+{
+
+	int assigned[WORKSET_TEST_SIZE] = { 0 }, visited[WORKSET_TEST_SIZE] = { 0 };
+	int i = 0, r = 0, c = 0, r0 = 0;
+
+	assert(sizeof(struct workset_data) == CACHE_LINE_SIZE);
+
+	if (WORKSET_TEST_SIZE == 1) {
+		ws[0].n = 0;
+
+		return;
+	}
+
+
+	r = random() % WORKSET_TEST_SIZE;
+	visited[r] = 1;
+	c = random() % WORKSET_TEST_SIZE;
+	assigned[c] = 1;
+	assert(r != c);
+	ws[r].n = c;
+	//printf("%d => %d,%d\n", i, r, c);
+	i++;
+	r0 = r;
+	r = c;
+
+	while (i < WORKSET_TEST_SIZE) {
+		c = random() % WORKSET_TEST_SIZE;
+
+		//printf("%d => %d,%d\n", i, r, c);
+		assert(visited[r] == 0);
+		if (visited[c] || assigned[c]) continue;
+
+		visited[r] = 1;
+		assigned[c] = 1;
+		ws[r].n = c;
+		r = c;
+
+		i++;
+		if (i == WORKSET_TEST_SIZE - 1) {
+			ws[r].n = r0;
+			break;
+		}
+	};
+
+	//for (i = 0; i < WORKSET_TEST_SIZE; i++) printf("%llu\n", ws[i].n);
+}
+
+volatile unsigned long sum;
+
+static void __attribute__((noinline))
+access_workset(void)
+{
+	unsigned long long i = 0;
+	int j = 0;
+	unsigned long s = 0;
+
+	i = ws[0].n;
+	j++;
+
+	while (j < WORKSET_TEST_SIZE) {
+		i = ws[i].n;
+		s += i;
+		j++;
+	}
+	sum = s;
+}
+
+void
+write_fn(int fd[]) 
+{
+	char ch = 'a';
+	int i;
+
+	for (i = 0 ; i < ITERS ; i ++) {
+		access_workset();
+		if (write(fd[1], &ch, 1) < 0) {
+			perror("write");
+			/* TODO: kill child! */
+			exit(-1);
+		}
+	}
+}
+
+void
+read_fn(int fd[])
+{
+	int i;
+	char ch;
+	unsigned long long r_total = 0, r_start, r_end, r_worst = 0;
+
+	for (i = 0 ; i < ITERS ; i ++) {
+		if (read(fd[0], &ch, 1) < 0) {
+			perror("read");
+			_exit(-1);
+		}
+		r_start = rdtsc();
+		access_workset();
+		r_end = rdtsc();
+		if ((r_end - r_start) > r_worst) r_worst = r_end - r_start;
+		r_total += (r_end - r_start);
+	}
+	printf("Passive Cost (Workset:%d, sz:%d) AVERAGE: %llu, WORST: %llu\n", WORKSET_TEST_SIZE, WORKSET_TEST_SIZE * CACHE_LINE_SIZE, (r_total)/(ITERS), r_worst);
+
+#ifndef USE_FORK
+	pthread_exit(NULL);
+#endif
+}
+
+
 int
 main(int argc, char **argv)
 {
 	int pipe_fd[2];
 	pid_t child;
-	unsigned long long w_total, w_start, w_end, r_total, r_start, r_end, r_worst = 0;
-	char ch = 'a';
+	pthread_t thd;
+
+	printf("generate\n");
+	generate_workset();
+	printf("..done\n");
+	printf("access\n");
+	access_workset();
+	printf("..done\n");
 
 	if (pipe(pipe_fd) < 0) {
 		perror("pipe");
 		return -1;
 	}
 
+#ifdef USE_FORK
+	printf("Using processes\n");
 	child = fork();
 	if (child > 0) {
-		int i;
-
 		set_prio(PARENT_PRIO, PARENT_CPU);
 		assert(sched_getcpu() == PARENT_CPU);
 		close(pipe_fd[0]);
@@ -53,14 +187,7 @@ main(int argc, char **argv)
 		/* making sure read blocks for predictability */
 		usleep(USEC_WAIT); 
 
-		for (i = 0 ; i < ITERS ; i ++) {
-			printf("W\n");
-			if (write(pipe_fd[1], &ch, 1) < 0) {
-				perror("write");
-				/* TODO: kill child! */
-				exit(-1);
-			}
-		}
+		write_fn(pipe_fd);
 		close(pipe_fd[1]);
 		wait(NULL);
 		exit(0);
@@ -70,26 +197,25 @@ main(int argc, char **argv)
 		set_prio(CHILD_PRIO, CHILD_CPU);
 		assert(sched_getcpu() == CHILD_CPU);
 		close(pipe_fd[1]);
-		r_total = 0;
 
-		for (i = 0 ; i < ITERS ; i ++) {
-			r_start = rdtsc();
-			printf("R\n");
-			if (read(pipe_fd[0], &ch, 1) < 0) {
-				perror("read");
-				_exit(-1);
-			}
-			r_end = rdtsc();
-			if ((r_end - r_start) > r_worst) r_worst = r_end - r_start;
-			r_total += (r_end - r_start);
-		}
-		printf("read/write AVERAGE: %llu, WORST: %llu\n", (r_total)/(2 * ITERS), r_worst);
+		read_fn(pipe_fd);
+
 
 		close (pipe_fd[0]);
 		_exit(0);
 	} else {
 		perror("fork");
 	}
+#else
+	printf("Using threads\n");
+
+	pthread_create(&thd, NULL, read_fn, pipe_fd);
+	pthread_prio(thd, CHILD_PRIO);
+	pthread_yield();
+	set_prio(PARENT_PRIO, PARENT_CPU);
+	
+	write_fn(pipe_fd);
+#endif
 
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
